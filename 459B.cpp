982914c759481529415fd649b8c6b74ff49aea5f
ujsquared <ujsquared@gmail.com>
2024-07-12 23:55:57 +0530

    452 && 12/07/2024

--- 459B.cpp
+++ 459B.cpp
@@ -5,32 +5,24 @@ using namespace std;
 typedef long long ll;
 int main (int argc, char *argv[]) {
     ll n; cin >> n;
-    ll max = INT32_MIN;
-    ll min = INT32_MAX;
-    ll maxdiff = 0;
-    ll countMax = 0;
-    ll countMin = 0;
-    while(n--){
-        ll x; cin >> x;
-        if( x >= max ){
-            if (x != max) {
-            countMax =1;
-            max  = x;
-        }else{
-            countMax += 1;
-        }
-        }
-        if( x <= min){
-            if(x!= min){
-                countMin = 1;
-                min  = x;
-            }else{
-                countMin++;
-            }
-        }
-        maxdiff = max - min;
-    }
-    cout << maxdiff << " " << countMin *  countMax;
+    ll maxb = 0;
+    ll minb = INT_MAX ;
+    ll minCount = 0;
+    ll maxCount = 0;
+    ll arr[n];
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+        minb = min(arr[i] , minb);
+        maxb = max(arr[i], maxb);
 
+    }
+    for(int i = 0; i < n; i++){
+        if(arr[i] == minb) minCount++;
+        else if(arr[i] == maxb) maxCount++;
+    }
+    if (minb == maxb)cout << 0 << " " << (n*(n-1))/2;
+    else{
+        cout << maxb - minb << " " << minCount * maxCount;
+    }
     return 0;
 }
